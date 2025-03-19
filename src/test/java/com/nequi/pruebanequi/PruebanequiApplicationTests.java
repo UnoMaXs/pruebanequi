967dcf7f0850e78f2eeb5a91f119e93a822e3436
Aiken Rojas <pieraiken202@hotmail.com>
2025-03-19 17:37:06 -0500

    Initial commit

---
+++ src/test/java/com/nequi/pruebanequi/PruebanequiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nequi.pruebanequi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebanequiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
