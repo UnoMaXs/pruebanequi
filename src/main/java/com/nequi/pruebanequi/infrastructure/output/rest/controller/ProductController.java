b93f506a2d6f79381329306af7528cb4b207f194
Aiken Rojas <pieraiken202@hotmail.com>
2025-03-20 11:35:40 -0500

    configuracion docker y swagger

--- src/main/java/com/nequi/pruebanequi/infrastructure/output/rest/controller/ProductController.java
+++ src/main/java/com/nequi/pruebanequi/infrastructure/output/rest/controller/ProductController.java
@@ -5,6 +5,12 @@ import com.nequi.pruebanequi.application.handler.impl.ProductHandlerImpl;
 import com.nequi.pruebanequi.domain.model.models.Product;
 import com.nequi.pruebanequi.domain.usecase.exceptions.BusinessException;
 import com.nequi.pruebanequi.infrastructure.output.rest.constants.Constants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +20,25 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/api" + Constants.PATH_PRODUCT)
 @RequiredArgsConstructor
+@Tag(name = "Product", description = "Endpoints para gestionar productos")
 public class ProductController {
 
     private final ProductHandlerImpl productHandler;
 
     @PostMapping
-    public Mono<ResponseEntity<Product>> createProduct(@RequestBody ProductRequestDTO requestDto) {
+    @Operation(
+            summary = "Crear un producto",
+            description = "Este endpoint permite crear un nuevo producto.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Producto creado exitosamente",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = Product.class))),
+                    @ApiResponse(responseCode = "400", description = "Error de negocio, producto no creado"),
+                    @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+            }
+    )
+    public Mono<ResponseEntity<Product>> createProduct(
+            @RequestBody @Parameter(description = "Datos necesarios para crear el producto") ProductRequestDTO requestDto) {
         return productHandler.createProduct(requestDto)
                 .map(product -> ResponseEntity.status(Constants.STATUS_CREATED).body(product))
                 .onErrorResume(BusinessException.class,
@@ -29,7 +48,16 @@ public class ProductController {
     }
 
     @DeleteMapping(Constants.PATH_DELETE_PATTERN)
-    public Mono<ResponseEntity<Void>> deleteProduct(@PathVariable(Constants.PATH_DELETE_VARIABLE) Integer productId) {
+    @Operation(
+            summary = "Eliminar un producto",
+            description = "Este endpoint permite eliminar un producto dado su ID.",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Producto eliminado exitosamente"),
+                    @ApiResponse(responseCode = "404", description = "Producto no encontrado")
+            }
+    )
+    public Mono<ResponseEntity<Void>> deleteProduct(
+            @PathVariable(Constants.PATH_DELETE_VARIABLE) @Parameter(description = "ID del producto a eliminar") Integer productId) {
         return productHandler.deleteProduct(productId)
                 .thenReturn(ResponseEntity.status(HttpStatus.NO_CONTENT).<Void>build())
                 .onErrorResume(BusinessException.class,
