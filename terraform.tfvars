d90486fa47093c8a830a81fe87c05e827b929faf
pierre.rojas_pragma <pier.rojas@pragma.com.co>
2025-03-20 16:05:07 -0500

    actualizar region

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
-aws_region     = "us-east-1"
+aws_region     = "us-east-2"
 vpc_cidr_block = "10.0.0.0/16"
 subnet_ids     = ["subnet-abc123", "subnet-def456"]
 db_username    = "usuarionequi"
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "Región de AWS donde se desplegarán los recursos"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 variable "vpc_cidr_block" {
